3b7d3160106f74ea9ea6bf8c19219393a0a9f584
BocchiKesselring <127914592+BocchiKesselring@users.noreply.github.com>
2023-10-28 19:18:54 -0700

    uwu

---
+++ scrip.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.ver-mas');
+    buttons.forEach(button => {
+      const details = button.getAttribute('data-details');
+      button.addEventListener('click', function() {
+        const popup = document.createElement('div');
+        popup.className = 'popup';
+        popup.innerHTML = details;
+        button.parentNode.appendChild(popup);
+        button.removeEventListener('click', this);
+        document.addEventListener('click', function(event) {
+          if (!popup.contains(event.target) && !button.contains(event.target)) {
+            popup.style.display = 'none';
+          }
+        });
+        popup.style.display = 'block';
+      });
+    });
+  });
+
+  
+  
+  
